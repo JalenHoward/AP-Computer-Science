763086facd5d89953d9be431a356ff6c25e94e35
JalenTheDev <jayhoward0108@hotmail.com>
2018-09-14 00:42:55 -0400

    First Project + Homework Upload
    Includes "Hello World" Project upload as well as HW 1-1 and 1-2

---
+++ Projects/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// System.out.println outputs lines within ""
+		System.out.print(5);
+		// println specifies new line, while print keeps all items on same line
+		System.out.print(16);
+		//Notice that the 16 is printed right next to the 5 as no new line is created
+
+	}
+
+}
