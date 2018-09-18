0b1d92493ee8aa06b1f9c2caa7e2cf265d4f7be3
JalenTheDev <jayhoward0108@hotmail.com>
2018-09-18 16:49:12 -0400

    Fixed Variables Project

--- Projects/VariablesProject/src/Variables.java
+++ Projects/VariablesProject/src/Variables.java
@@ -7,7 +7,7 @@ public class Variables {
 		int B = 5; //gives value to integer B while defining it, the value given is 5
 		int C, D; //defines integer C and D
 		A = 4; //gives a value to integer A of 4
-		C = A + B; //gives the value of C as the combined total of integers A and B
+		C = A / B; //gives the value of C as the combined total of integers A and B
 		D = 0; //gives a value of 0 to integer D
 		D = D + 1; //Adds 1 to the previously defined value of D
 		
