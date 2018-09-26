bcdd170f6adb8e40afbce7eb81db58f97a2465e7
JalenTheDev <jayhoward0108@hotmail.com>
2018-09-26 13:45:51 -0400

    Update to Classwork 1-2
    updated comments

--- Projects/Classwork 1-2/src/Classwork12.java
+++ Projects/Classwork 1-2/src/Classwork12.java
@@ -10,15 +10,15 @@ public class Classwork12 {
 	
 	if (+ a > 0) //if variable a is greater than or equal to 0, it is positive
 	{
-		System.out.println(+ a +" is positive"); // prints out this message if true
+		System.out.println(+ a +" is positive"); // prints out this message if the number is positive
 	}
 	else if (+ a < 0)
 	{
-		System.out.println(+ a + " is negative"); //prints out this message if false
+		System.out.println(+ a + " is negative"); //prints out this message if the number is a negative number
 	}
 	else
 	{
-		System.out.println(+ a + " is just 0");
+		System.out.println(+ a + " is just 0"); //prints out this message if the number is equal to 0
 	}
 	 number.close(); // closes Scanner "number"
 	}
