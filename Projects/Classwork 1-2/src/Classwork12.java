c4bd692382042074d19d60e74fe7bdb083c2c893
JalenTheDev <jayhoward0108@hotmail.com>
2018-09-26 13:44:26 -0400

    Updates to Classwork 1-2
    added an if/else if/else statement to the program

--- Projects/Classwork 1-2/src/Classwork12.java
+++ Projects/Classwork 1-2/src/Classwork12.java
@@ -6,15 +6,19 @@ public class Classwork12 {
 		
 	Scanner number =  new Scanner(System.in); //creates a new scanner named "number"
 	System.out.print("Enter a number ");
-	double a = number.nextDouble(); //creates a double defined by variable "a".
+	int a = number.nextInt(); //creates a double defined by variable "a".
 	
-	if (+ a >= 0) //if variable a is greater than or equal to 0, it is positive
+	if (+ a > 0) //if variable a is greater than or equal to 0, it is positive
 	{
 		System.out.println(+ a +" is positive"); // prints out this message if true
 	}
+	else if (+ a < 0)
+	{
+		System.out.println(+ a + " is negative"); //prints out this message if false
+	}
 	else
 	{
-		System.out.println(+ a + " is not positive"); //prints out this message if false
+		System.out.println(+ a + " is just 0");
 	}
 	 number.close(); // closes Scanner "number"
 	}
